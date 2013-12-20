f99d6d77f79b8e8f3e3260d0f0a5bd45cd0b84cd
X <X@X.com>
2013-12-19 20:10:28 -0800

    distance on/off

--- app.js
+++ app.js
@@ -6,16 +6,42 @@ var
 board.on('ready', function() {
 	
 	var 
-		blue = new five.Led(3)
-		, white = new five.Led(5)
+		white = new five.Led(3)
+		, green = new five.Led(5)
 	;
 
-	blue.fader = fader.bind(blue);
+	/*blue.fader = fader.bind(blue);
 	white.fader = fader.bind(white);
 
 	blue.fader();
-	white.fader();
+	white.fader();*/
 
+  var ping = new five.Ping({
+  pin: 7,
+  freq: 50,
+  pulse: 50});
+
+  ping.on("change", function( err, value ) {
+  	/*if(this.cm < 10){
+  		green.strobe(this.cm * this.cm * 10)
+  		white.strobe(this.cm * this.cm * 10)
+  	} else {
+  		green.off();
+  		white.off();
+  	}*/
+
+  	if(this.cm > 10 && this.cm < 20)
+  		green.on();
+  	else
+  		green.off();
+
+  	if(this.cm > 20 )
+  		white.on();
+  	else
+  		white.off();
+    console.log('Object is ' + this.cm + ' cm away');
+    //console.log('Object is ' + this.inches + ' inches away');
+  });
 });
 
 function fader() {
